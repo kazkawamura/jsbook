2617774f699c0e5800e5b3b5c0fbcf2f0711db86
Kazuyuki Kawamura <kaz.kawamura@gmail.com>
2012-01-10 08:29:57 +0900

    update async sample

--- node.js/hello2sec.js
+++ node.js/hello2sec.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   setTimeout(function() {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("Hello World\n");
   }, 2000);
 }).listen(8888);
