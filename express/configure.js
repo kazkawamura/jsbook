c042949b9472fe724eb3b1fc6a2ed7c4f1434a91
Kazuyuki Kawamura <kaz.kawamura@gmail.com>
2012-01-29 22:46:02 +0900

    add config sample

---
+++ express/configure.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express.createServer();
+app.configure(function() {
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
+app.configure('development', function() {
+  app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+});
+app.configure('production', function() {
+  app.use(express.errorHandler());
+});
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+app.listen(8888);
