daf06c7b8a65756a797ac14ec59b3a72c723b6b5
Kazuyuki Kawamura <kaz.kawamura@gmail.com>
2012-03-04 22:35:15 +0900

    update static.js to support directory listing

--- express/static.js
+++ express/static.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express.createServer();
 app.use(express.static(__dirname + '/public', {maxAge: 864000000}));
 app.use(express.staticCache());
+app.use(express.directory(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.send('Hello World');
 });
