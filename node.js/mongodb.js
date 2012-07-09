2d98cc49b1be3be2bd9757157c2117d0d2b5121e
Kazuyuki Kawamura <kaz.kawamura@gmail.com>
2012-07-10 08:29:02 +0900

    Add MongoDB Sample

---
+++ node.js/mongodb.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var url = require('url');
+var mongo = require('mongodb');
+http.createServer(function (req, res) {
+  var firstname = url.parse(req.url, true).query['firstname'];
+  var lastname = url.parse(req.url, true).query['lastname'];
+  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+  res.write('Hello ' + firstname + '\n');
+  var server = new mongo.Server('localhost', 27017, {auto_reconnect: true});
+  var db = new mongo.Db('userDirectory', server);
+  db.open(function(err, db) {
+    db.collection('members', function(err, collection) {
+      var doc1 = {'firstname': firstname, 'lastname': lastname};
+      collection.insert(doc1, function(err, result) {
+        var stream = collection.find().streamRecords();
+        stream.on('data',  function(item) {
+          res.write(item.firstname + ' ' + item.lastname + '\n');
+        });
+        stream.on('end',  function() {
+          res.end();
+        });
+      });
+    });
+  });
+}).listen(8888);
+console.log('Server running at http://127.0.0.1:8888/');
